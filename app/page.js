31ae02fb25c2f0d882ac6609c5478a570ffb1cd5
bold ariunbold <ariunboldbold200@gmail.com>
2024-05-14 18:59:17 +0800

    fix responsive

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function MainPage() {
       <div
         ref={ref}
         style={mainPipeState.observing ? { opacity: 1 } : { opacity: 0 }}
-        className="w-[100vw] h-[90vh] flex justify-center gap-[170px] items-center duration-[1400ms] "
+        className="header w-[100vw] h-[90vh] flex justify-center gap-[170px] items-center duration-[1400ms] "
       >
         <div className="flex flex-col text-center">
           <p className="text-[50px] font-extrabold">hi, im Bold Ariunbold</p>
@@ -57,11 +57,11 @@ export default function MainPage() {
             alt="profile picture"
             width={400}
             height={400}
-            className="duration-300 childDivProfile rounded-[20px] "
+            className="duration-500 childDivProfile rounded-[20px] "
           />
         </div>
       </div>
-      <div className="flex flex-row items-center text-center">
+      <div className="flex gaper flex-row items-center text-center">
         <p>projects</p>
         <div
           style={{
@@ -74,7 +74,7 @@ export default function MainPage() {
         ></div>
       </div>
       <div className="flex-col items-center w-[100vw] h-[55vh] flex flex-wrap gap-[20px]">
-        <div className="flex flex-row gap-[10px]">
+        <div className="flex flex-row gap-[10px] projects-container">
           {projectDatas.map((e, key) => {
             return (
               <Projects
@@ -86,7 +86,7 @@ export default function MainPage() {
             );
           })}
         </div>
-        <div className="flex flex-row items-center">
+        <div className="flex flex-row gaper items-center">
           <p>Side Projects</p>
           <div
             style={{
@@ -102,18 +102,6 @@ export default function MainPage() {
           <a href="https://github.com/simp6969/rock-paper-scissors">
             &#8226; simple terminal rock paper scissors
           </a>
-        </div>
-        <div className="flex flex-row-reverse items-center text-center">
-          <div
-            style={{
-              borderTop: "2px solid #323232 ",
-              marginLeft: 20,
-              marginRight: 20,
-              height: "1px",
-              width: "80vw",
-            }}
-          ></div>
-          <p>about</p>
         </div>
       </div>
       <div
