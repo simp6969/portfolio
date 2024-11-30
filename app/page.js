466aadef94b47fa3f71f3eab6422781b1c1baaaf
bold ariunbold <ariunboldbold200@gmail.com>
2024-11-30 22:23:31 +0800

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,24 +1,195 @@
 "use client";
 
-import { useEffect } from "react";
-import { MainHeader } from "./components/MainHeader";
-import { MainSolvingCards } from "./components/MainSolvingCards";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards, Navigation } from "swiper/modules";
+import "swiper/css/effect-cards";
+import "swiper/css";
+import { useEffect, useRef, useState } from "react";
 
-export default function Play() {
+export default function LandingPage() {
+  const [state, setState] = useState({
+    popup: false,
+    isCardPageIntersecting: false,
+  });
+  const style = { "--font-color": "#4a2e31" };
+  const cardRef = useRef();
   useEffect(() => {
-    localStorage.setItem(
-      "theme",
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light"
-    );
-    let body = document.getElementsByTagName("body");
-    body.item(0).classList.add(localStorage.getItem("theme"));
+    if (state.popup) {
+      setTimeout(() => {
+        setState({ ...state, popup: false });
+      }, 3000);
+    }
+  }, [state.popup]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((e) => {
+      const entry = e[0];
+      if (entry.isIntersecting) {
+        setState({ ...state, isCardPageIntersecting: true });
+      } else {
+        setState({ ...state, isCardPageIntersecting: true });
+      }
+    });
+    observer.observe(cardRef.current);
   }, []);
+
   return (
-    <div className="w-[100vw] h-[100vh]">
-      {/* <MainHeader /> */}
-      <MainSolvingCards />
+    <div className="landingPageDesign">
+      {state.popup && (
+        <div className="popup">
+          <svg
+            height={20}
+            width={20}
+            fill="#e2cdbf"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+          </svg>
+          <h1 className="defaultHeader !text-[16px]">
+            Successfully copied email
+          </h1>
+        </div>
+      )}
+      <div
+        className={
+          state.isCardPageIntersecting
+            ? "poppingOutAnimation swiper-button image-swiper-button-next"
+            : "swiper-button image-swiper-button-next"
+        }
+      >
+        <svg
+          height={40}
+          width={40}
+          fill="#9f6369"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg>
+      </div>
+      <div
+        className={
+          state.isCardPageIntersecting
+            ? "swiper-button image-swiper-button-prev poppingOutAnimation"
+            : "swiper-button image-swiper-button-prev"
+        }
+      >
+        <svg
+          height={40}
+          width={40}
+          fill="#9f6369"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+        </svg>
+      </div>
+      <Swiper
+        grabCursor={true}
+        ref={cardRef}
+        navigation={{
+          nextEl: ".image-swiper-button-next",
+          prevEl: ".image-swiper-button-prev",
+          disabledClass: "swiper-button-disabled",
+        }}
+        onActiveIndexChange={(e) => console.log(e.activeIndex)}
+        className={
+          state.isCardPageIntersecting ? "cards poppingOutAnimation" : "cards"
+        }
+        modules={[EffectCards, Navigation]}
+        effect="Cards"
+      >
+        {/* <svg
+          height={30}
+          width={30}
+          fill="#e2cdbf"
+          className="relative"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg> */}
+        <SwiperSlide className="card justify-center">
+          <h1 className="defaultHeader w-[36%] gradTitle">
+            Hi, my name is Bold Ariunbold
+          </h1>
+
+          <div className="w-[256px] h-[256px]">
+            <img
+              className="rounded-[10px] profile_image"
+              src="./favicon.ico"
+            ></img>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide
+          style={style}
+          className="card justify-center flex-col bg-[#e2cdbf]"
+        >
+          <h1 className="defaultHeader flex w-[95%] justify-end text-center">
+            As a full-stack developer , I create exceptional user experiences.
+          </h1>
+          <div className="flex gap-[20px]">
+            <svg
+              height={30}
+              width={30}
+              fill="#4a2e31"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              onClick={() => {
+                window.open(
+                  "https://www.linkedin.com/in/ariunbold-bold-60058b30a/"
+                );
+              }}
+            >
+              <title>Linked In</title>
+              <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+            </svg>
+            <svg
+              eight={30}
+              width={30}
+              fill="#4a2e31"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 496 512"
+              onClick={() => {
+                window.open("https://github.com/simp6969");
+              }}
+            >
+              <title>Github</title>
+              <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+            </svg>
+            <svg
+              eight={30}
+              width={30}
+              fill="#4a2e31"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              onClick={() => {
+                setState({ ...state, popup: true });
+                navigator.clipboard.writeText("ariunboldbold200@gmail.com");
+              }}
+            >
+              <title>Copy email</title>
+              <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+            </svg>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div
+        onClick={() => {}}
+        className="absolute cursor-pointer bottom-[20px] right-[50%] flex items-center flex-col"
+      >
+        <h1 className="defaultHeader !text-[20px] !text-[#4a2e31] ">Project</h1>
+        <svg
+          height={30}
+          width={30}
+          fill="#4a2e31"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+        >
+          <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+        </svg>
+      </div>
     </div>
   );
 }
