1066c3773cbb1440c2c8f9497781cbbfab6f471e
bold ariunbold <ariunboldbold200@gmail.com>
2024-12-16 21:08:22 +0800

    color combo fix :>

---
+++ app/components/ProjectsPage.jsx
@@ -0,0 +1,9 @@
+export function ProjectsPage() {
+  return (
+    <div className="landingPageDesign">
+      <h1 className="defaultHeader">
+        <div className="">Projects</div>
+      </h1>
+    </div>
+  );
+}
