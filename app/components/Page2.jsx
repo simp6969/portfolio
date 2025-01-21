5a099e854bfda430465a55a571ea2ae8e2ccf9dc
bold ariunbold <ariunboldbold200@gmail.com>
2025-01-21 13:32:04 +0800

    page 1

---
+++ app/components/Page2.jsx
@@ -0,0 +1,10 @@
+export function Page2() {
+  return (
+    <div className="bg-[url(../public/page2Gradient.svg)] bg-cover bg-no-repeat h-[100vh] w-[100vw] text-[#3A5673] flex justify-center items-center">
+      <div>
+        <img src="../public/goals.jpg"></img>
+        <h1>Future Goals</h1>
+      </div>
+    </div>
+  );
+}
