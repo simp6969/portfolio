f370051f2c856e77c1a8b0c1ac7f851992251dba
Hu Tao <ariunboldbold200@gmail.com>
2025-02-22 13:30:09 +0800

    update on page1

--- app/page.js
+++ app/page.js
@@ -1,101 +1,65 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Page1 } from "@/components/Page1";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [state, setState] = useState({});
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setState({ intersect: true });
+        } else {
+          setState({ intersect: false });
+        }
+      });
+    });
+    const tar1 = document.getElementById("target1");
+    observer.observe(tar1);
+  }, [!state.intersect]);
+  // useEffect(() => {
+  //   if (window) {
+  //     const observer = new IntersectionObserver((entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting) {
+  //           setState({ intersect1: true });
+  //         } else {
+  //           setState({ intersect1: false });
+  //         }
+  //       });
+  //     });
+  //     const tar = document.getElementById("target1");
+  //     observer.observe(tar);
+  //   }
+  // }, [!state.intersect1]);
+  console.log(state);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="justify-center items-center h-[100vh] w-[100vw] gap-[20px] page1 flex">
+        <div
+          id="target1"
+          className={state.intersect ? "para" : "flex gap-[10px] flex-col"}
+        >
+          <h1>Nice to meet you, im Ariunbold Bold</h1>
+          <div className="flex gap-[20px]">
+            <button className="defaultButton bg-[var(--secondaryColor)]">
+              Contact
+            </button>
+            <button className="defaultButton !text-[var(--secondaryColor)] border-4 border-[var(--secondaryColor)]">
+              Projects
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div
+          id="target1"
+          className={state.intersect ? "main_img" : "main_img1"}
+        />
+      </div>
+      <div className="w-[100vw] h-[100vh]"></div>
+      <div className="w-[100vw] introPage h-[100vh]">
+        <Page1 />
+      </div>
     </div>
   );
 }
